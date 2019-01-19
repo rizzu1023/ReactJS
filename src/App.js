fc23ddfd7509f80d277634421ba9c3f0453d3e3d
rizzu1023 <mrizwan9022@gmail.com>
2019-01-19 23:17:53 +0530

    create app & components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Todos from './components/Todos';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id : 1,
+        title : "Don't critisize & condemn or complain",
+        completed : false
+      },
+      {
+        id : 2,
+        title : "Genuinly intrest in other people's opinion",
+        completed : false
+      },
+      {
+        id : 3,
+        title : "they will check just your presence of mind",
+        completed : false
+      }
+    ]
+  }
+
   render() {
+        // console.log(this.state.todos)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Todos todos={this.state.todos} />
       </div>
+
     );
   }
 }
